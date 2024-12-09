eecadc46882fa04e8faa4170a21bf5d354b076ee
jesperls <jesper.ls@hotmail.com>
2024-12-10 00:55:45 +0100

    Chapter 4 and flowcharts
    Huge commit adding the content for chapter 4.
    It also adds the obsidian flowcharts compiled into html files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,26 @@ export class AppComponent {
   title = 'GenFound';
   selectedChapter: string = '1-0';
   showAlphabet: boolean = false;
+  selectedMain: string = '';
+  obsidianUrl: SafeResourceUrl = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
+
+  selectMain(option: string) {
+    this.selectedMain = option;
+    this.selectedChapter = '';
+    this.showAlphabet = option === 'Alphabet';
+    this.obsidianUrl = '';
+  }
 
   selectChapter(chapter: string) {
-    this.showAlphabet = false;
-    this.selectedChapter = chapter;
+    if (this.selectedMain === 'Chapters') {
+      this.selectedChapter = chapter;
+      this.showAlphabet = false;
+    } else if (this.selectedMain === 'Obsidian') {
+      const url = `assets/Obsidian/Chapter${chapter}/Chapter${chapter}.html`;
+      this.obsidianUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    }
   }
 
   selectAlphabet() {
